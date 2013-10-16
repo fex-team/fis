c49388afb52b68bc2aa1cade651ae69ba1a7f5c8
tianlili <tianlili@baidu.com>
2013-10-16 17:23:59 +0800

    [change]增加fis插件

--- test/util/diff/new_fis_ready.sh
+++ test/util/diff/new_fis_ready.sh
@@ -5,4 +5,5 @@ FIS_PATH=/home/work/repos/fis/
 cd ${FIS_PATH}                                                                                                                                npm install
 npm install
 npm install fis-preprocessor-inline
+npm install fis-preprocessor-extlang
 sh ${TEST_PATH}/release.sh new
