a05cc360bdcda44e66c740b3a98f2efb2e9ba67f
753643789 <wangfangguo@baidu.com>
2014-06-17 19:04:10 +0800

    【autopack测试脚本】 自动打包测试脚本

--- test/util/autopack/start.sh
+++ test/util/autopack/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+if [ -d "./result" ]; then
 rm -r result
+fi
 wget -O a.txt  "http://solar.baidu.com/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
 wget -O b.txt  "http://fedev.baidu.com:8803/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
 if [ ! -d "./result" ]; then
