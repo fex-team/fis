208e9739d46ebd92e4cdf5c2a92a9ccc660d3ead
753643789 <wangfangguo@baidu.com>
2014-06-18 17:08:24 +0800

    【autopack测试脚本】 自动打包测试脚本

--- test/util/auto-plugin/start.sh
+++ test/util/auto-plugin/start.sh
@@ -6,6 +6,7 @@ mkdir svntest
 cd ./svntest
 svn co https://svn.baidu.com/app/global/hao123/branches/fe/flat-home/hao123_1-0-988_BRANCH ./hao123 --username wangfangguo --password WOAImingtian123
 svn co https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH ./tuangou --username wangfangguo --password WOAImingtian123
+ls
 cd ./hao123
 fisp release -p -d ./output
 cd ..
