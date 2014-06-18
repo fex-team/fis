5671af8f5f38c23b5fd7eaf0a1257b0dc9cc5b5b
753643789 <wangfangguo@baidu.com>
2014-06-18 16:30:40 +0800

    【autopack测试脚本】 自动打包测试脚本

--- test/util/auto-plugin/start.sh
+++ test/util/auto-plugin/start.sh
@@ -7,3 +7,14 @@ cd ./svntest
 svn co https://svn.baidu.com/app/global/hao123/branches/fe/flat-home/hao123_1-0-988_BRANCH ./hao123 --username wangfangguo --password WOAImingtian123
 svn co https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH ./tuangou --username wangfangguo --password WOAImingtian123
 cd ..
+if [ ! -d "./result" ]; then
+mkdir result
+fi
+php TestAutoPlugin.php
+npm uninstall -g fis-postpackager-ext-map
+npm uninstall -g fis-packager-autopack
+npm uninstall -g fis-parser-bainuo-less
+rm -r svntest
+if [ -d "./result" ]; then
+rm -r result
+fi
