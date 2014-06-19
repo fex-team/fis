ef0a5921f85e402816df7b5b29deecaf76917a56
753643789 <wangfangguo@baidu.com>
2014-06-19 10:08:14 +0800

    【autopack测试脚本】 自动打包测试脚本

--- test/util/auto-plugin/start.sh
+++ test/util/auto-plugin/start.sh
@@ -4,9 +4,10 @@ npm install -g fis-packager-autopack
 npm isntall -g fis-parser-bainuo-less
 mkdir svntest
 cd ./svntest
-svn co https://svn.baidu.com/app/global/hao123/branches/fe/flat-home/hao123_1-0-988_BRANCH ./hao123 --username wangfangguo --password WOAImingtian123
-svn co https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH ./tuangou --username wangfangguo --password WOAImingtian123
-ls
+mkdir hao123
+mkdir tuangou
+svn co  --username wangfangguo --password WOAImingtian123 https://svn.baidu.com/app/global/hao123/branches/fe/flat-home/hao123_1-0-988_BRANCH ./hao123
+svn co  --username wangfangguo --password WOAImingtian123 https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH ./tuangou
 cd ./hao123
 fisp release -p -d ./output
 cd ..
