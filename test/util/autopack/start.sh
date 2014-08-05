2e401d7e74c59df2ca2d882774756ec96c375e34
753643789 <wangfangguo@baidu.com>
2014-08-05 09:35:44 +0800

    【autopack测试脚本】 更新生成结果文件

--- test/util/autopack/start.sh
+++ test/util/autopack/start.sh
@@ -5,8 +5,8 @@ npm isntall -g fis-parser-bainuo-less
 if [ -d "./result" ]; then
 rm -r result
 fi
-wget -O a.txt  "http://solar.baidu.com/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
-wget -O b.txt  "http://fedev.baidu.com:8803/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
+wget -O a.txt  "http://fedev.baidu.com:8803/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
+wget -O b.txt  "http://solar.baidu.com/autopack?return=json&fid=baidunuomi&svn=https://svn.baidu.com/app/search/tuangou/branches/mobile-bainuo/common/tuangou_4-3-102_BRANCH"
 if [ ! -d "./result" ]; then
   mkdir result
 fi
