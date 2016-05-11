ff23a24935a1b863fdc16d859976bfa3ae94c7ff
Sighless <hefangshi@gmail.com>
2016-05-11 15:42:47 +0800

    use local svn auth cache

--- test/util/diff/product_code.sh
+++ test/util/diff/product_code.sh
@@ -21,11 +21,11 @@ HAO123_DIR=./product_code/hao123
 SUPERMAN_SVN=https://svn.baidu.com/app/search/lbs-webapp/trunk/mmap/superman
 SUPERMAN_DIR=./product_code/superman
 
-svn co --username=$1 --password=$2 --no-auth-cache ${PLACE_SVN} ${PLACE_DIR}
-svn co --username=$1 --password=$2 --no-auth-cache ${BATMAN_SVN} ${BATMAN_DIR}
-svn co --username=$1 --password=$2 --no-auth-cache ${WENKU_SVN} ${WENKU_DIR}
-svn co --username=$1 --password=$2 --no-auth-cache ${TIEBA_SVN} ${TIEBA_DIR}
-svn co --username=$1 --password=$2 --no-auth-cache ${HAO123_SVN}/common ${HAO123_DIR}/common
-svn co --username=$1 --password=$2 --no-auth-cache ${HAO123_SVN}/home ${HAO123_DIR}/home
-svn co --username=$1 --password=$2 --no-auth-cache ${HAO123_SVN}/lv2 ${HAO123_DIR}/lv2
-svn co --username=$1 --password=$2 --no-auth-cache ${SUPERMAN_SVN} ${SUPERMAN_DIR}
+svn co ${PLACE_SVN} ${PLACE_DIR}
+svn co ${BATMAN_SVN} ${BATMAN_DIR}
+svn co ${WENKU_SVN} ${WENKU_DIR}
+svn co ${TIEBA_SVN} ${TIEBA_DIR}
+svn co ${HAO123_SVN}/common ${HAO123_DIR}/common
+svn co ${HAO123_SVN}/home ${HAO123_DIR}/home
+svn co ${HAO123_SVN}/lv2 ${HAO123_DIR}/lv2
+svn co ${SUPERMAN_SVN} ${SUPERMAN_DIR}
